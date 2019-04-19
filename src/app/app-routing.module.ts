c168019f80469bdc8e23040c3efecc3e73cc0f00
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2019-04-19 16:16:51 -0300

    se muestran las tecnologias utilizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { GithubComponent } from './pages/github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: MainComponent },
+  { path: "github", component: GithubComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MainComponent } from './pages/main/main.component';
+import { GithubComponent } from './pages/github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
